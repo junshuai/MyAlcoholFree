3a0a584411ea201f4c483b1bf73c957019deadda
Junshuai Zhang <juz1@aber.ac.uk>
2015-11-02 18:44:34 +0000

    Update database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+Product.create!(
+  title: 'Eminasin Tempranillo',
+  description: 'A rich, clear, red with purple hues, this wine has aromas of summer fruits with hints of cherry and strawberry against an earthy background of licorice, coffee, vanilla and tobacco.',
+  image_url: 'http://www.alcoholfree.co.uk/images/emina_red_thumb180.jpg',
+  price: 6.99
+)
+Product.create!(
+  title: 'Organic Cabernet Sauvignon',
+  description: 'A new organic wine from Weinkonig. This is a dark, full-bodied red wine with a fruity bouquet of blackberry and very delicate oak note. Well balanced tannins provide drinking pleasure.',
+  image_url: 'http://www.alcoholfree.co.uk/images/cab_sauv_bio_thumb180.jpg',
+  price: 7.99
+)
