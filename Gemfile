14ea02754bc2d2750584dbc5a28303ecfebed472
Junshuai Zhang <juz1@aber.ac.uk>
2015-12-01 19:28:16 +0000

    Use PostgreSQL instead of sqlite3 for deployment of heroku

--- Gemfile
+++ Gemfile
@@ -3,8 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# Use postgresql as the database for Active Record
+gem 'pg'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -46,3 +46,6 @@ group :development do
 end
 
 gem 'will_paginate', '~> 3.0.6'
+
+gem 'rails_12factor', group: :production
+ruby "2.2.0"
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151130201450) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 20151130201450) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "customers", ["user_id"], name: "index_customers_on_user_id"
+  add_index "customers", ["user_id"], name: "index_customers_on_user_id", using: :btree
 
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -38,9 +41,9 @@ ActiveRecord::Schema.define(version: 20151130201450) do
     t.integer  "order_id"
   end
 
-  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
-  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id"
-  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
+  add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
+  add_index "line_items", ["order_id"], name: "index_line_items_on_order_id", using: :btree
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.string   "name"
@@ -65,7 +68,7 @@ ActiveRecord::Schema.define(version: 20151130201450) do
     t.datetime "updated_at",                                         null: false
   end
 
-  add_index "product_details", ["product_id"], name: "index_product_details_on_product_id"
+  add_index "product_details", ["product_id"], name: "index_product_details_on_product_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -85,4 +88,9 @@ ActiveRecord::Schema.define(version: 20151130201450) do
     t.string   "role"
   end
 
+  add_foreign_key "customers", "users"
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
+  add_foreign_key "product_details", "products"
 end
