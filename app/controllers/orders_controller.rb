d384ef0e683ad4300a2e17189f3dce545aee4399
Junshuai Zhang <juz1@aber.ac.uk>
2015-12-06 14:13:29 +0000

    Attempt to fix a bug only appeard on heroku

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
-    @order.user_id = @user.id
+    @order.user = @user
     @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
